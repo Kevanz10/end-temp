576a274a5a861849c338e14253d9b6a4da99f082
Carlos Alzate <carlos.alzate@endava.com>
2021-08-05 17:17:25 -0500

    Benefit type delete option

--- src/shared/utilities/Utils.js
+++ src/shared/utilities/Utils.js
@@ -7,22 +7,62 @@ export const fixEmailInput = e => {
     const [el, inputType] = [e.target, e.inputType];
     const containsEndava = el.value.indexOf('endava.com');
 
-    if (e.data === ' ') {
-        e.preventDefault()
-        el.value = el.value.trim()
-    }
+    console.log(e);
 
-    if (inputType === 'deleteContentBackward') {
+    if (inputType === 'deleteContentBackward' || inputType === 'insertReplacementText') {
         return;
     }
 
+    if (inputType === 'insertFromPaste') {
+        el.value = '';
+    }
+
+    if (e.data === ' ') {
+        el.value = el.value.replace(/\s/g, '');
+        e.preventDefault()
+    }
+
     if (containsEndava !== -1) {
-        el.value = (e.data === ' ') ? el.value : el.value.slice(0, -1);
+        el.value = (e.data === ' ') ? el.value.trim() : el.value.slice(0, -1);
     }
 
     if (el.value.indexOf('@') !== -1) {
-        if (containsEndava === -1) {
-            el.value += 'endava.com'
+
+        const emailParts = el.value.split('@');
+
+        if (emailParts[1].length > 0) {
+            el.value = (e.data === ' ') ? el.value.trim() : el.value.slice(0, -1);
+        } else {
+            if (containsEndava === -1) {
+                el.value += 'endava.com'
+            }
         }
     }
+}
+
+export const setPageTitle = (title, authState) => {
+    const titleParts = title.split('|');
+    let newTitle = ''
+    switch (authState) {
+        case 'signup':
+            newTitle = titleParts[0] + '| Create your account'
+            break;
+        case 'signin':
+            newTitle = titleParts[0] + '| Log in to your account'
+            break;
+        case 'forgotpassword':
+            newTitle = titleParts[0] + '| Reset your account'
+            break;
+        case 'confirmSignUp':
+            newTitle = titleParts[0] + '| Confirm your new account'
+            break;
+        case 'signedin':
+            newTitle = titleParts[0] + '| Manage your benefits'
+            break;
+
+        default:
+            break;
+
+    }
+    return newTitle;
 }
